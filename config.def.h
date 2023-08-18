45994298e63e94c40493e24c89c3d58efbb6253d
Maico Tedesco <maicotedesco@gmail.com>
2023-08-18 16:12:56 -0300

    add more stuff

--- config.def.h
+++ config.def.h
@@ -7,31 +7,32 @@
 #define BROWCLASS "Brave-browser"
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const unsigned int borderpx           = 2;        /* border pixel of windows */
+static const unsigned int snap               = 32;       /* snap pixel */
+static const unsigned int gappih             = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv             = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh             = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov             = 10;       /* vert outer gap between windows and screen edge */
+static       int smartgaps                   = 1;        /* 1 means no outer gap when there is only one window */
+static const int swallowfloating             = 0;        /* 1 means swallow floating windows by default */
+static const unsigned int systraypinning     = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft      = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing     = 2;   /* systray spacing */
+static const int systraypinningfailfirst     = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray                 = 1;        /* 0 means no systray */
+static const int showbar                     = 1;        /* 0 means no bar */
+static const int topbar                      = 1;        /* 0 means bottom bar */
+static const char *fonts[]                   = { "Noto Sans Mono:size=10", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
+static char normbgcolor[]                    = "#222222";
+static char normbordercolor[]                = "#444444";
+static char normfgcolor[]                    = "#bbbbbb";
+static char selfgcolor[]                     = "#eeeeee";
+static char selbordercolor[]                 = "#005577";
+static char selbgcolor[]                     = "#005577";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 typedef struct {
@@ -135,15 +136,30 @@ static const Key keys[] = {
 	TAGKEYS(                      XK_9,                            8)
 	STACKKEYS(MODKEY,                             focus)
 	STACKKEYS(MODKEY|ShiftMask,                   push)
+	{ MODKEY,                     XK_w,           spawn,           {.v = (const char*[]){ BROWSER, NULL } } },
+    { MODKEY|ShiftMask,           XK_w,           spawn,           {.v = (const char*[]){ TERMINAL, "-e", "sudo", "nmtui", NULL } } },
+	{ MODKEY,                     XK_q,           killclient,      {0} },
+	{ MODKEY|ShiftMask,           XK_q,           spawn,           {.v = (const char*[]){ "sysact", NULL } } },
+	{ MODKEY,                     XK_r,           spawn,           {.v = (const char*[]){ TERMINAL, "-e", "lfub", NULL } } },
+	{ MODKEY|ShiftMask,           XK_r,           spawn,           {.v = (const char*[]){ TERMINAL, "-e", "htop", NULL } } },
+    { MODKEY,                     XK_t,           setlayout,       {.v = &layouts[0]} }, /* tile */
+    { MODKEY|ShiftMask,           XK_t,           setlayout,       {.v = &layouts[1]} }, /* bstack */
+    { MODKEY,                     XK_y,           setlayout,       {.v = &layouts[2]} }, /* spiral */
+    { MODKEY|ShiftMask,           XK_y,           setlayout,       {.v = &layouts[3]} }, /* dwindle */
+    { MODKEY,                     XK_u,           setlayout,       {.v = &layouts[4]} }, /* deck */
+    { MODKEY|ShiftMask,           XK_u,           setlayout,       {.v = &layouts[5]} }, /* monocle */
+    { MODKEY,                     XK_i,           setlayout,       {.v = &layouts[6]} }, /* centeredmaster */
+    { MODKEY|ShiftMask,           XK_i,           setlayout,       {.v = &layouts[7]} }, /* centeredfloatingmaster */
+	{ MODKEY,                     XK_o,           incnmaster,      {.i = +1 } },
+	{ MODKEY|ShiftMask,           XK_o,           incnmaster,      {.i = -1 } },
+    { MODKEY,                     XK_p,           spawn,           {.v = (const char*[]){ "mpc", "toggle", NULL } } },
+    { MODKEY|ShiftMask,           XK_p,           spawn,           SHCMD("mpc pause; pauseallmpv") },
     { MODKEY,                     XK_d,           spawn,           {.v = (const char*[]){ "dmenu_run", NULL } } },
     { MODKEY|ShiftMask,           XK_d,           spawn,           {.v = (const char*[]){ "passmenu", NULL } } },
 	{ MODKEY,                     XK_Return,      spawn,           {.v = termcmd } },
 	{ MODKEY|ShiftMask,           XK_Return,      zoom,            {0} },
 	{ MODKEY,                     XK_b,           togglebar,       {0} },
-	{ MODKEY,                     XK_i,           incnmaster,      {.i = +1 } },
-	{ MODKEY,                     XK_p,           incnmaster,      {.i = -1 } },
-	{ MODKEY,                     XK_r,           spawn,           {.v = (const char*[]){ TERMINAL, "-e", "lfub", NULL } } },
-	{ MODKEY|ShiftMask,           XK_r,           spawn,           {.v = (const char*[]){ TERMINAL, "-e", "htop", NULL } } },
+    { MODKEY|ShiftMask,           XK_b,           toggleborder,    {0} },
 	{ MODKEY,                     XK_h,           setmfact,        {.f = -0.05} },
 	{ MODKEY,                     XK_l,           setmfact,        {.f = +0.05} },
 	{ MODKEY|Mod1Mask,            XK_u,           incrgaps,        {.i = +1 } },
@@ -152,9 +168,6 @@ static const Key keys[] = {
 	{ MODKEY|Mod1Mask|ShiftMask,  XK_0,           defaultgaps,     {0} },
 
 	{ MODKEY,                     XK_Tab,         view,            {0} },
-	{ MODKEY,                     XK_t,           setlayout,       {.v = &layouts[0]} },
-	{ MODKEY,                     XK_f,           setlayout,       {.v = &layouts[1]} },
-	{ MODKEY,                     XK_m,           setlayout,       {.v = &layouts[2]} },
     { MODKEY,                     XK_g,           shiftview,       { .i = -1 } },
     { MODKEY|ShiftMask,           XK_g,           shifttag,        { .i = -1 } },
     { MODKEY,                     XK_semicolon,   shiftview,       { .i = 1 } },
@@ -175,16 +188,26 @@ static const Key keys[] = {
 	{ MODKEY,                     XK_Right,       focusmon,        {.i = +1 } },
 	{ MODKEY|ShiftMask,           XK_Left,        tagmon,          {.i = -1 } },
 	{ MODKEY|ShiftMask,           XK_Right,       tagmon,          {.i = +1 } },
+    
+    /****** Functions Keys ******/  
+	/* modifier         Keys        function      argument */
+    { MODKEY,           XK_F1,      spawn,        SHCMD("groff -mom /usr/local/share/dwm/larbs.mom -Tpdf | zathura -") },
+	{ MODKEY,           XK_F3,      spawn,	      {.v = (const char*[]){ "displayselect", NULL } } },
+	{ MODKEY,           XK_F4,      spawn,	      SHCMD(TERMINAL " -e pulsemixer; kill -44 $(pidof dwmblocks)") },
+	{ MODKEY,           XK_F5,      xrdb,	      {.v = NULL } },
+	{ MODKEY,           XK_F6,      spawn,	      {.v = (const char*[]){ "torwrap", NULL } } },
+	{ MODKEY,           XK_F7,      spawn,	      {.v = (const char*[]){ "td-toggle", NULL } } },
+	{ MODKEY,           XK_F8,      spawn,	      {.v = (const char*[]){ "mailsync", NULL } } },
+	{ MODKEY,           XK_F9,      spawn,	      {.v = (const char*[]){ "mounter", NULL } } },
+	{ MODKEY,           XK_F10,     spawn,	      {.v = (const char*[]){ "unmounter", NULL } } },
+	{ MODKEY,           XK_F11,     spawn,	      SHCMD("mpv --untimed --no-cache --no-osc --no-input-default-bindings --profile=low-latency --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
+	{ MODKEY,           XK_F12,	    spawn,	      SHCMD("remaps") },
 
-	{ MODKEY,                     XK_w,           spawn,           {.v = (const char*[]){ BROWSER, NULL } } },
-    { MODKEY|ShiftMask,           XK_w,           spawn,           {.v = (const char*[]){ TERMINAL, "-e", "sudo", "nmtui", NULL } } },
-	{ MODKEY,                     XK_q,           killclient,      {0} },
-	{ MODKEY|ShiftMask,           XK_q,           spawn,           {.v = (const char*[]){ "sysact", NULL } } },
-
-    /* XF86XK */  
+    /****** XF86XK ******/  
+	/* modifier                     function      argument */
     { 0, XF86XK_AudioMute,          spawn,        SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof dwmblocks)") },
-    { 0, XF86XK_AudioRaiseVolume,   spawn,        SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%+; kill -44 $(pidof dwmblocks)") },
-    { 0, XF86XK_AudioLowerVolume,   spawn,        SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%-; kill -44 $(pidof dwmblocks)") },
+    { 0, XF86XK_AudioRaiseVolume,   spawn,        SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 0%- && wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%+; kill -44 $(pidof dwmblocks)") },
+    { 0, XF86XK_AudioLowerVolume,   spawn,        SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 0%- && wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%-; kill -44 $(pidof dwmblocks)") },
     { 0, XF86XK_AudioPrev,          spawn,        {.v = (const char*[]){ "mpc", "prev", NULL } } },
     { 0, XF86XK_AudioNext,          spawn,        {.v = (const char*[]){ "mpc",  "next", NULL } } },
     { 0, XF86XK_AudioPause,         spawn,        {.v = (const char*[]){ "mpc", "pause", NULL } } },
--- dwm.c
+++ dwm.c
@@ -35,6 +35,7 @@
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
 #include <X11/Xproto.h>
+#include <X11/Xresource.h>
 #include <X11/Xutil.h>
 #ifdef XINERAMA
 #include <X11/extensions/Xinerama.h>
@@ -71,6 +72,21 @@
 #define SPTAGMASK	        	(((1 << LENGTH(scratchpads))-1) << LENGTH(tags))
 #define TEXTW(X)                (drw_fontset_getwidth(drw, (X)) + lrpad)
 #define TRUNC(X,A,B)            (MAX((A), MIN((X), (B))))
+#define XRDB_LOAD_COLOR(R,V)    if (XrmGetResource(xrdb, R, NULL, &type, &value) == True) { \
+                                  if (value.addr != NULL && strnlen(value.addr, 8) == 7 && value.addr[0] == '#') { \
+                                    int i = 1; \
+                                    for (; i <= 6; i++) { \
+                                      if (value.addr[i] < 48) break; \
+                                      if (value.addr[i] > 57 && value.addr[i] < 65) break; \
+                                      if (value.addr[i] > 70 && value.addr[i] < 97) break; \
+                                      if (value.addr[i] > 102) break; \
+                                    } \
+                                    if (i == 7) { \
+                                      strncpy(V, value.addr, 7); \
+                                      V[7] = '\0'; \
+                                    } \
+                                  } \
+                                }
 
 #define SYSTEM_TRAY_REQUEST_DOCK    0
 /* XEMBED messages */
@@ -224,6 +240,7 @@ static void grabkeys(void);
 static void incnmaster(const Arg *arg);
 static void keypress(XEvent *e);
 static void killclient(const Arg *arg);
+static void loadxrdb(void);
 static void manage(Window w, XWindowAttributes *wa);
 static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
@@ -265,6 +282,7 @@ static int stackpos(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void togglebar(const Arg *arg);
+static void toggleborder(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void togglefullscr(const Arg *arg);
 static void togglescratch(const Arg *arg);
@@ -293,6 +311,7 @@ static Client *wintosystrayicon(Window w);
 static int xerror(Display *dpy, XErrorEvent *ee);
 static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
+static void xrdb(const Arg *arg);
 static void zoom(const Arg *arg);
 
 static pid_t getparentprocess(pid_t p);
@@ -1296,6 +1315,37 @@ killclient(const Arg *arg)
 }
 
 void
+loadxrdb()
+{
+  Display *display;
+  char * resm;
+  XrmDatabase xrdb;
+  char *type;
+  XrmValue value;
+
+  display = XOpenDisplay(NULL);
+
+  if (display != NULL) {
+    resm = XResourceManagerString(display);
+
+    if (resm != NULL) {
+      xrdb = XrmGetStringDatabase(resm);
+
+      if (xrdb != NULL) {
+        XRDB_LOAD_COLOR("dwm.normbordercolor", normbordercolor);
+        XRDB_LOAD_COLOR("dwm.normbgcolor", normbgcolor);
+        XRDB_LOAD_COLOR("dwm.normfgcolor", normfgcolor);
+        XRDB_LOAD_COLOR("dwm.selbordercolor", selbordercolor);
+        XRDB_LOAD_COLOR("dwm.selbgcolor", selbgcolor);
+        XRDB_LOAD_COLOR("dwm.selfgcolor", selfgcolor);
+      }
+    }
+  }
+
+  XCloseDisplay(display);
+}
+
+void
 manage(Window w, XWindowAttributes *wa)
 {
 	Client *c, *t = NULL, *term = NULL;
@@ -1620,12 +1670,26 @@ void
 resizeclient(Client *c, int x, int y, int w, int h)
 {
 	XWindowChanges wc;
+	unsigned int n;
+	Client *nbc;
 
 	c->oldx = c->x; c->x = wc.x = x;
 	c->oldy = c->y; c->y = wc.y = y;
 	c->oldw = c->w; c->w = wc.width = w;
 	c->oldh = c->h; c->h = wc.height = h;
 	wc.border_width = c->bw;
+
+	for (n = 0, nbc = nexttiled(c->mon->clients); nbc; nbc = nexttiled(nbc->next), n++);
+
+	if (c->isfloating || c->mon->lt[c->mon->sellt]->arrange == NULL) {
+	} else {
+		if (c->mon->lt[c->mon->sellt]->arrange == monocle || n == 1) {
+			wc.border_width = 0;
+			c->w = wc.width += c->bw * 2;
+			c->h = wc.height += c->bw * 2;
+		}
+	}
+
 	XConfigureWindow(dpy, c->win, CWX|CWY|CWWidth|CWHeight|CWBorderWidth, &wc);
 	configure(c);
 	XSync(dpy, False);
@@ -2140,6 +2204,12 @@ togglebar(const Arg *arg)
 	}
 	arrange(selmon);
 }
+void
+toggleborder(const Arg *arg)
+{
+  selmon->sel->bw = (selmon->sel->bw == borderpx ? 0 : borderpx);
+  arrange(selmon);
+}
 
 void
 togglefloating(const Arg *arg)
@@ -2308,7 +2378,7 @@ updatebars(void)
 		w = m->ww;
 		if (showsystray && m == systraytomon(m))
 			w -= getsystraywidth();
-		m->barwin = XCreateWindow(dpy, root, m->wx, m->by, w, bh, 0, DefaultDepth(dpy, screen),
+		m->barwin = XCreateWindow(dpy, root, m->wx, m->by, m->ww, bh, 0, DefaultDepth(dpy, screen),
 				CopyFromParent, DefaultVisual(dpy, screen),
 				CWOverrideRedirect|CWBackPixmap|CWEventMask, &wa);
 		XDefineCursor(dpy, m->barwin, cursor[CurNormal]->cursor);
@@ -2903,6 +2973,17 @@ systraytomon(Monitor *m) {
 }
 
 void
+xrdb(const Arg *arg)
+{
+  loadxrdb();
+  int i;
+  for (i = 0; i < LENGTH(colors); i++)
+                scheme[i] = drw_scm_create(drw, colors[i], 3);
+  focus(NULL);
+  arrange(NULL);
+}
+
+void
 zoom(const Arg *arg)
 {
 	Client *c = selmon->sel;
@@ -2928,6 +3009,8 @@ main(int argc, char *argv[])
 	if (!(xcon = XGetXCBConnection(dpy)))
 		die("dwm: cannot get xcb connection\n");
 	checkotherwm();
+        XrmInitialize();
+        loadxrdb();
 	setup();
 #ifdef __OpenBSD__
 	if (pledge("stdio rpath proc exec ps", NULL) == -1)
